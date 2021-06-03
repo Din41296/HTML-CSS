841ba5db96f0a9a843dc2353341f1f3d090541fe
Dina96 <>
2021-06-03 11:23:26 +0000

    Editing

---
+++ script.js
@@ -0,0 +1,20 @@
+var userName = "Dina Muasher";
+
+document.write("<p> I wish you were excited in my website " + "Dina Muasher </p>");
+
+alert("Welcome to your second home");
+
+var  Book1 = "Fiction";
+var Book2 = "Adventure";
+var Book3 = "Historical";
+
+var FavBook = prompt("Kindly write your favorite book");
+if(FavBook == Book1) {
+  alert("Press on Fiction");
+} else if(FavBook != Book3) {
+  alert("Press on Adventure");
+
+} else {
+  alert("Press on Historical")
+}
+ 
